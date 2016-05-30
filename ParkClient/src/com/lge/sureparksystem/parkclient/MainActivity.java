48000f8f46263e2753921dfe78c12b6aa3a69b08
kimoon.lee <kimoon.lee@kimoonlee2.LGE.NET>
2016-05-31 08:36:50 +0900

    Build error fix

--- ParkClient/src/com/lge/sureparksystem/parkclient/MainActivity.java
+++ ParkClient/src/com/lge/sureparksystem/parkclient/MainActivity.java
@@ -1,15 +1,30 @@
 package com.lge.sureparksystem.parkclient;
 
-import android.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// add comment
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
