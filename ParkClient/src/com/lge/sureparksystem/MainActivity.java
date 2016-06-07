7f757c2d6ad60c67e12bbd54c918f95ca7eab70c
kimoon.lee <kimoon.lee@kimoonlee2.LGE.NET>
2016-06-08 05:44:46 +0900

    Modify login view (Add Create ID button)

--- ParkClient/src/com/lge/sureparksystem/MainActivity.java
+++ ParkClient/src/com/lge/sureparksystem/MainActivity.java
@@ -13,11 +13,16 @@ import com.lge.sureparksystem.control.NetworkManager;
 import com.lge.sureparksystem.parkclient.R;
 import com.lge.sureparksystem.util.Utils;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
 
     private static String TAG = "ParkClientActivity";
     private View mLoadingView;
     private NetworkManager mNetworkManager;
+    private AbstractFactory mLoginFactory;
+    private AbstractFactory mOwnerFactory;
+    private AbstractFactory mAttendantFactory;
+    private AbstractFactory mDriverFactory;
+    private int mTempNum = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +32,33 @@ public class MainActivity extends Activity implements OnClickListener{
         mLoadingView.setVisibility(View.VISIBLE);
         // mNetworkManager = new NetworkManager();
         findViewById(R.id.btn_temp).setOnClickListener(this);
+        initFactory();
+    }
+
+    private void initFactory() {
+        mLoginFactory = new LoginFactory();
+        mLoginFactory.createModel();
+        mLoginFactory.createView();
+
+        mOwnerFactory = new OwnerFactory();
+        mOwnerFactory.createModel();
+        mOwnerFactory.createView();
+
+        mAttendantFactory = new AttendantFactory();
+        mAttendantFactory.createModel();
+        mAttendantFactory.createView();
+
+        mDriverFactory = new DriverFactory();
+        mDriverFactory.createModel();
+        mDriverFactory.createView();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
-        AbstractFactory loginFactory = new AttendantFactory();
-        loginFactory.createModel();
-        loginFactory.createView();
-        ft.replace(R.id.fragment_area, (Fragment)loginFactory.mBaseView, Utils.TAG_LOGIN_FRAGMENT);
+        ft.replace(R.id.fragment_area, (Fragment)mLoginFactory.mBaseView,
+                Utils.TAG_LOGIN_FRAGMENT);
         ft.commitAllowingStateLoss();
         findViewById(R.id.fragment_area).setVisibility(View.VISIBLE);
         mLoadingView.setVisibility(View.GONE);
@@ -65,7 +87,7 @@ public class MainActivity extends Activity implements OnClickListener{
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-         getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -78,8 +100,35 @@ public class MainActivity extends Activity implements OnClickListener{
         return super.onOptionsItemSelected(item);
     }
 
+    private void switchFagment(AbstractFactory factory, String fragment_tag) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_area, (Fragment)factory.mBaseView,
+                fragment_tag);
+        ft.commitAllowingStateLoss();
+    }
+
     @Override
     public void onClick(View v) {
-//        getFragmentManager()
+        int num = mTempNum % 4;
+        String tag = null;
+        AbstractFactory af = null;
+        if (num >= 4) {
+            num = 0;
+        }
+        if (num == 0) {
+            af = mLoginFactory;
+            tag = Utils.TAG_LOGIN_FRAGMENT;
+        } else if (num == 1) {
+            af = mAttendantFactory;
+            tag = Utils.TAG_ATTENDANT_FRAGMENT;
+        } else if (num == 2) {
+            af = mOwnerFactory;
+            tag = Utils.TAG_OWNER_FRAGMENT;
+        } else {
+            af = mDriverFactory;
+            tag = Utils.TAG_DRIVER_FRAGMENT;
+        }
+        switchFagment(af, tag);
+        mTempNum++;
     }
 }
