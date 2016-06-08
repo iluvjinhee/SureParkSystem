6159954dbffb544d4c74d843871174a90b331aaa
iluvjinhee <yongchul.park@lge.com>
2016-06-08 10:41:34 -0400

    SocketMessage jar 생성 및 Client 공유

--- ParkServer/src/com/lge/sureparksystem/parkserver/reservationmanager/ReservationManager.java
+++ ParkServer/src/com/lge/sureparksystem/parkserver/reservationmanager/ReservationManager.java
@@ -1,5 +1,15 @@
 package com.lge.sureparksystem.parkserver.reservationmanager;
 
 public class ReservationManager {
+	public boolean isValid(String confirmationNumber) {
+		return isValidEmailAddress(confirmationNumber);
+	}
 
+	public static boolean isValidEmailAddress(String email) {
+		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+		java.util.regex.Matcher m = p.matcher(email);
+		
+		return m.matches();
+	}
 }
