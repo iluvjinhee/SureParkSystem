88464844209624f76dcba3aa920bf2b0cf321fce
iluvjinhee <yongchul.park@lge.com>
2016-06-15 09:26:06 -0400

    AES 암호화 클래스 추가

---
+++ ParkServer/src/com/lge/sureparksystem/parkserver/manager/securitymanager/AES.java
@@ -0,0 +1,27 @@
+package com.lge.sureparksystem.parkserver.manager.securitymanager;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES {
+	static String IV = "AAAAAAAAAAAAAAAA";
+	static String plaintext = "test text 123\0\0\0"; /* Note null padding */
+	static String encryptionKey = "0123456789abcdef";
+
+	public static byte[] encrypt(String plainText, String encryptionKey) throws Exception {
+		Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+		cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		
+		return cipher.doFinal(plainText.getBytes("UTF-8"));
+	}
+
+	public static String decrypt(byte[] cipherText, String encryptionKey) throws Exception {
+		Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+		cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		
+		return new String(cipher.doFinal(cipherText), "UTF-8");
+	}
+}
