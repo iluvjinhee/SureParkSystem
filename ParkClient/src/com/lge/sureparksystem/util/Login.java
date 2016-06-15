f0ab8cfd3316f9497f762054fb4f1ca6aba5f881
kimoon.lee <kimoon.lee@kimoonlee2.LGE.NET>
2016-06-16 00:53:39 +0900

    Interface definition upload

---
+++ ParkClient/src/com/lge/sureparksystem/util/Login.java
@@ -0,0 +1,10 @@
+package com.lge.sureparksystem.util;
+
+public class Login {
+    // ParkHere to ParkServer
+    // authority number : owner 1, attendant 2, driver 3
+    // 1. createAccount(Email, pw, createTime(ms), authority) : String result
+    class CreateAccount {
+        String result;
+    }
+}
