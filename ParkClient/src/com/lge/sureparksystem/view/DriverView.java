c55fee1e4dda07153c397b0d1ed919b0addf23a2
kimoon.lee <kimoon.lee@kimoonlee2.LGE.NET>
2016-06-16 03:35:31 +0900

    Add base owner view

--- ParkClient/src/com/lge/sureparksystem/view/DriverView.java
+++ ParkClient/src/com/lge/sureparksystem/view/DriverView.java
@@ -9,7 +9,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.lge.sureparksystem.parkclient.R;
 import com.lge.sureparksystem.util.Utils;
@@ -19,6 +22,10 @@ public class DriverView extends Fragment implements BaseView {
     private static final String TAG = "DriverView";
     private Spinner mParkIDSppiner;
     private Spinner mReservationTimeSppiner;
+    private LinearLayout mArrivedTimelayout;
+    private TextView mReservedTime;
+    private Button mButton;
+    private boolean mIsReservedUser = false; // Check if reserved info exist
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,14 +36,19 @@ public class DriverView extends Fragment implements BaseView {
         mParkIDSppiner = (Spinner)result.findViewById(R.id.park_id);
         mReservationTimeSppiner = (Spinner)result
                 .findViewById(R.id.reservation_time);
+        mArrivedTimelayout = (LinearLayout)result
+                .findViewById(R.id.arrived_time);
+        mReservedTime = (TextView)result.findViewById(R.id.reserved_time);
+        mButton = (Button) result.findViewById(R.id.btn_ok);
         return result;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        CharSequence[] parkId = {"Pittsburgh", "Chicago"};
+        CharSequence[] parkId = { "Pittsburgh", "Chicago" };
         ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(
-                getActivity().getApplicationContext(), android.R.layout.simple_spinner_item, parkId);
+                getActivity().getApplicationContext(),
+                android.R.layout.simple_spinner_item, parkId);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mParkIDSppiner.setAdapter(adapter);
         mParkIDSppiner.setOnItemSelectedListener(new OnItemSelectedListener() {
@@ -44,7 +56,8 @@ public class DriverView extends Fragment implements BaseView {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                     int position, long id) {
-                Utils.showToast(getActivity(), "position : " + position + ", id : " + id);
+                Utils.showToast(getActivity(), "position : " + position
+                        + ", id : " + id);
             }
 
             @Override
@@ -52,25 +65,30 @@ public class DriverView extends Fragment implements BaseView {
                 Utils.showToast(getActivity(), "unselected");
             }
         });
-        
-        CharSequence[] reservationTime = {"After 1 hour", "After 2 hours", "After 3 hours"};
+
+        CharSequence[] reservationTime = { "After 1 hour", "After 2 hours",
+                "After 3 hours" };
         ArrayAdapter<CharSequence> adapterReserve = new ArrayAdapter<CharSequence>(
-                getActivity().getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, reservationTime);
-        adapterReserve.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                getActivity().getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, reservationTime);
+        adapterReserve
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mReservationTimeSppiner.setAdapter(adapterReserve);
-        mReservationTimeSppiner.setOnItemSelectedListener(new OnItemSelectedListener() {
+        mReservationTimeSppiner
+                .setOnItemSelectedListener(new OnItemSelectedListener() {
 
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
-                    int position, long id) {
-                Utils.showToast(getActivity(), "position : " + position + ", id : " + id);
-            }
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent,
+                            View view, int position, long id) {
+                        Utils.showToast(getActivity(), "position : " + position
+                                + ", id : " + id);
+                    }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Utils.showToast(getActivity(), "unselected");
-            }
-        });
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        Utils.showToast(getActivity(), "unselected");
+                    }
+                });
 
         super.onActivityCreated(savedInstanceState);
     }
@@ -82,6 +100,15 @@ public class DriverView extends Fragment implements BaseView {
 
     @Override
     public void onResume() {
+        if (mIsReservedUser) {
+            mReservedTime.setVisibility(View.VISIBLE);
+            mArrivedTimelayout.setVisibility(View.GONE);
+            mButton.setText(R.string.cancel);
+        } else {
+            mReservedTime.setVisibility(View.GONE);
+            mArrivedTimelayout.setVisibility(View.VISIBLE);
+            mButton.setText(R.string.ok);
+        }
         super.onResume();
     }
 
